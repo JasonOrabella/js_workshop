5f3938c169c020915c76bbdd66e45b9956cb848a
Jason Orabella <jasonorabella@gmail.com>
2020-08-04 19:25:01 -0400

    final product, made the js more modular moved const's to top of the js file the buttons all work

--- index.js
+++ index.js
@@ -1,40 +1,37 @@
- // devloper mozzila resource 
-const header = document.querySelector('header');
-
-console.log("verify header", header);
-
-
-const headerTextContainer = document.querySelector ('.header__text_container');
-
+const deleteGreetingButton = document.querySelector('#header__button_delete');
 const greetButton = document.querySelector('#header__button_greeting');
-
-
-function greetuser(){
-//create paragraph element
-const paragraph = document.createElement ('p')
-//add text to this paragraph element "hello
-paragraph.innerText = 'hello';
-// place 
-headerTextContainer.appendChild(paragraph);
-}
-
-greetButton.addEventListener('click',greetuser);
-
-const deleteGreetingButton = document.querySelector('#header__button_delete')
-
-deleteGreetingButton.addEventListener('click',()=>{
-    headerTextContainer.innerHTML = null;
-})
+const header = document.querySelector('.header');
+const headerTextContainer = document.querySelector('.header__text_container');
 const image = document.querySelector('.image');
-const imageTextContainer = document.querySelector('.image__text_container')
-
-image.addEventListener('click',()=>{
-    if(imageTextContainer.children.length <1){
-        const paragraph = document.createElement('p')
-        paragraph.innerText = 'This computer is a great buy';
-        imageTextContainer.appendChild(paragraph);
-    }
-    else {
-        imageTextContainer = null;
-    }
-})
+const imageTextContainer = document.querySelector('.image__text_container');
+//this is what will fire in the browser
+renderPage();
+function renderPage() {
+    renderHeaderInfo();
+    renderImageInfo();
+}
+function renderHeaderInfo() {
+    greetButton.addEventListener('click', () => {
+        createParagraph(headerTextContainer, 'Hello');
+    });
+    deleteGreetingButton.addEventListener('click', () => {
+        removeParagraph(headerTextContainer);
+    })
+}
+function renderImageInfo() {
+    image.addEventListener('click', () => {
+        if (imageTextContainer.children.length < 1) {
+            createParagraph(imageTextContainer, 'This computer is a great buy!');
+        } else {
+            removeParagraph(imageTextContainer);
+        }
+    })
+}
+function createParagraph(parentElement, textToDisplay) {
+    const paragraph = document.createElement('p');
+    paragraph.innerText = textToDisplay;
+    parentElement.appendChild(paragraph);
+}
+function removeParagraph(parentElement) {
+    parentElement.innerHTML = null;
+}
