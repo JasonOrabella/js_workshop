8d42d8ee0d8225429a066ed85a74591d552c82e7
Jason Orabella <jasonorabella@gmail.com>
2020-08-04 18:29:00 -0400

    28 mins into workshop, started header

---
+++ index.js
@@ -0,0 +1,7 @@
+ // devloper mozzila resource 
+const header = document.querySelector('header');
+
+console.log("verify header", header);
+
+
+const headerTextContainer = document.querySelector ('header__text_container');
